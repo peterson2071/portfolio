7f8912afd374834682f5b630b91ad4650e7d0ae3
Pedro Reis <pedroaugustoreis10000@gmail.com>
2025-02-27 11:46:32 +0000

    Change in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [react()],
     server: {
         base: '/portfolio/'
     }
-}
+})
