461ce509f880ecc2dfddd89590f780306c38c41f
Pedro Reis <pedroaugustoreis10000@gmail.com>
2025-02-27 12:12:38 +0000

    newly again

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './vite.config.js';
 
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
